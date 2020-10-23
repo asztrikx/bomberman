4293e78d6423d43c84e18cafdd0b93262984b647
asztrikx <asztrik35@gmail.com>
2020-10-23 20:54:56 +0200

    added name

--- client.c
+++ client.c
@@ -2,6 +2,7 @@
 #include "client.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdlib.h>
 #include "SDL.h"
 #include "state.h"
@@ -84,6 +85,25 @@ void ClientConnect(void){
 }
 
 void ClientDraw(WorldClient* worldClient){
+	//character find
+	Character* characterMe = NULL;
+	for(int i=0; i<worldClient->characterSLength; i++){
+		if(worldClient->characterS[i].type == CharacterTypeYou){
+			characterMe = &(worldClient->characterS[i]);
+			break;
+		}
+	}
+	if(characterMe == NULL){
+		printf("I'm lost :(");
+		exit(1);
+	}
+
+	//offset
+	Position offset = (Position){
+		.y = -characterMe->position.y + windowHeight / 2,
+		.x = -characterMe->position.x + windowWidth / 2,
+	};
+
 	//clear & background
 	if(SDL_SetRenderDrawColor(SDLRenderer, 0, 255, 0, 255) < 0){
 		SDL_Log("SDL_SetRenderDrawColor: %s", SDL_GetError());
@@ -102,8 +122,8 @@ void ClientDraw(WorldClient* worldClient){
 		}
 
 		if(SDL_RenderFillRect(SDLRenderer, &(SDL_Rect){
-			.y = worldClient->exit->y,
-			.x = worldClient->exit->x,
+			.y = worldClient->exit->y + offset.y,
+			.x = worldClient->exit->x + offset.x,
 			.w = squaresize,
 			.h = squaresize,
 		}) < 0){
@@ -120,8 +140,8 @@ void ClientDraw(WorldClient* worldClient){
 		}
 
 		if(SDL_RenderFillRect(SDLRenderer, &(SDL_Rect){
-			.y = worldClient->objectS[i].position.y,
-			.x = worldClient->objectS[i].position.x,
+			.y = worldClient->objectS[i].position.y + offset.y,
+			.x = worldClient->objectS[i].position.x + offset.x,
 			.w = squaresize,
 			.h = squaresize,
 		}) < 0){
@@ -138,14 +158,52 @@ void ClientDraw(WorldClient* worldClient){
 		}
 
 		if(SDL_RenderFillRect(SDLRenderer, &(SDL_Rect){
-			.y = worldClient->characterS[i].position.y,
-			.x = worldClient->characterS[i].position.x,
+			.y = worldClient->characterS[i].position.y + offset.y,
+			.x = worldClient->characterS[i].position.x + offset.x,
 			.w = squaresize,
 			.h = squaresize,
 		}) < 0){
 			SDL_Log("SDL_RenderFillRect: %s", SDL_GetError());
 			exit(1);
 		}
+	}
+
+	//character name
+	for(int i=0; i<worldClient->characterSLength; i++){
+		/*if (stringRGBA(
+			SDLRenderer,
+			worldClient->characterS[i].position.x + offset.x,
+			worldClient->characterS[i].position.y + 10 + offset.y,
+			worldClient->characterS[i].name,
+			0, 0, 0, 255
+		) > 0){
+			SDL_Log("stringRGBA: %s", SDL_GetError());
+			exit(1);
+		}*/
+
+		TTF_Font* font = TTF_OpenFont("NotoSansMono-Regular.ttf", 24); //this opens a font style and sets a size
+		if(!font) {
+			printf("TTF_OpenFont: %s\n", TTF_GetError());
+			exit(1);
+		}
+		SDL_Color Black = {0, 0, 0};  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
+		SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, worldClient->characterS[i].name, Black); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
+		SDL_Texture* Message = SDL_CreateTextureFromSurface(SDLRenderer, surfaceMessage); //now you can convert it into a texture
+		SDL_Rect Message_rect; //create a rect
+		Message_rect.x = worldClient->characterS[i].position.x + offset.x;  //controls the rect's x coordinate 
+		Message_rect.y = worldClient->characterS[i].position.y - 50 + offset.y; // controls the rect's y coordinte
+		Message_rect.w = squaresize; // controls the width of the rect
+		Message_rect.h = 24; // controls the height of the rect
+
+		//Mind you that (0,0) is on the top left of the window/screen, think a rect as the text's box, that way it would be very simple to understand
+
+		//Now since it's a texture, you have to put RenderCopy in your game loop area, the area where the whole code executes
+
+		SDL_RenderCopy(SDLRenderer, Message, NULL, &Message_rect); //you put the renderer's name first, the Message, the crop size(you can ignore this if you don't want to dabble with cropping), and the rect which is the size and coordinate of your texture
+
+		//Don't forget to free your surface and texture
+		SDL_FreeSurface(surfaceMessage);
+		SDL_DestroyTexture(Message);
 	}
 
 	SDL_RenderPresent(SDLRenderer);
