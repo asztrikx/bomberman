2ceb6a7f37fdca6670cb2559e36f17ec058a01e9
asztrikx <asztrik35@gmail.com>
2020-10-26 17:21:07 +0100

    added helper functions, modified to use helper functions, added mutex, added missing frees

--- server.c
+++ server.c
@@ -5,13 +5,59 @@
 #include "geometry.h"
 #include "network.h"
 
+//mutex handles all global variable which is modified in critical sections
+static SDL_mutex* mutex;
 static UserServerItem* userServerItemS;
 static WorldServer* worldServer;
 static long long tickCount = 0;
 static unsigned int tickRate = 60u;
+static int tickId;
+
+UserServer* ServerAuthCheck(char* auth){
+	UserServerItem* userServerItemCurrent = userServerItemS;
+	while(userServerItemCurrent != NULL){
+		//connecting user
+		if(userServerItemCurrent->userServer.auth == NULL){
+			userServerItemCurrent = userServerItemCurrent->next;
+			continue;
+		}
+
+		//timing attack safe compare
+		bool diff = false;
+		for(int i=0; i<26; i++){
+			if(auth[i] != userServerItemCurrent->userServer.auth[i]){
+				diff = true;
+			}
+		}
+		if(!diff){
+			return &(userServerItemCurrent->userServer);
+		}
+
+		userServerItemCurrent = userServerItemCurrent->next;
+	}
+
+	return NULL;
+}
+
+//ServerAuthCreate creates a 26 character long auth key
+//return should be free'd by caller
+char* ServerAuthCreate(){
+	char* auth = (char*) malloc((26 + 1) * sizeof(char));
+	for(int i=0; i<26; i++){
+		auth[i] = rand() % ('Z' - 'A' + 1) + 'A';
+	}
+	auth[26] = '\0';
+
+	return auth;
+}
 
 //ServerTick calculates new frame, notifies users
 Uint32 ServerTick(Uint32 interval, void *param){
+	if(SDL_LockMutex(mutex) != 0){
+		SDL_Log("ServerTick: mutex lock: %s", SDL_GetError());
+		exit(1);
+	}
+
 	//[R]bomb explosion
 	//[R]player death
 	//[R]crate delete
@@ -19,6 +65,7 @@ Uint32 ServerTick(Uint32 interval, void *param){
 	//user notify
 	UserServerItem* userServerItemCurrent = userServerItemS;
 	while(userServerItemCurrent != NULL){
+		//alter user character to be identifiable
 		userServerItemCurrent->userServer.character->type = CharacterTypeYou;		
 		networkSendClient(worldServer);
 		userServerItemCurrent->userServer.character->type = CharacterTypeUser;
@@ -26,14 +73,35 @@ Uint32 ServerTick(Uint32 interval, void *param){
 		userServerItemCurrent = userServerItemCurrent->next;
 	}
 
+	if(SDL_UnlockMutex(mutex) < 0){
+		SDL_Log("ServerTick: mutex unlock: %s", SDL_GetError());
+		exit(1);
+	}
+
 	return interval;
 }
 
-//ServerStart generates world, start ticking
+//ServerStart generates world, start accepting connections, starts ticking
 void ServerStart(void){
-	worldServer = worldGenerate(11, 11);
+	//world generate
+	worldServer = worldGenerate(11, 11); //not critical section
 
-	SDL_AddTimer(1000u/tickRate, ServerTick, NULL);
+	//mutex init
+	mutex = SDL_CreateMutex();
+	if (!mutex){
+		SDL_Log("mutex create: %s", SDL_GetError());
+		exit(1);
+	}
+
+	//network start
+	networkServerStart();
+
+	//tick start: world calc, connected user update
+	tickId = SDL_AddTimer(1000u/tickRate, ServerTick, NULL);
+	if (tickId == 0){
+		SDL_Log("SDL_AddTimer: %s", SDL_GetError());
+		exit(1);
+	}
 }
 
 //keyMovement calculates user position based on keyItem.key
@@ -123,27 +191,16 @@ void keyBomb(SDL_Keycode key, UserServer* userServer){
 
 //ServerReceive gets updates from users
 void ServerReceive(UserServer* userServerUnsafe){
-	//auth validate
-	UserServerItem* userServerItemCurrent = userServerItemS;
-	while(userServerItemCurrent != NULL){
-		//timing attack safe compare
-		bool diff = false;
-		for(int i=0; i<26; i++){
-			if(userServerUnsafe->auth[i] != userServerItemCurrent->userServer.auth[i]){
-				diff = true;
-			}
-		}
-		if(!diff){
-			break;
-		}
-
-		userServerItemCurrent = userServerItemCurrent->next;
+	if (SDL_LockMutex(mutex) != 0){
+		puts("ServerReceive: mutex lock");
+		exit(1);
 	}
 
-	if(userServerItemCurrent == NULL){
+	//auth validate
+	UserServer* userServer = ServerAuthCheck(userServerUnsafe->auth); //[R] auth may be shorther than 26
+	if(userServer == NULL){
 		return;
 	}
-	UserServer* userServer = &(userServerItemCurrent->userServer);
 
 	//name change
 	if(
@@ -158,10 +215,20 @@ void ServerReceive(UserServer* userServerUnsafe){
 		keyBomb(userServerUnsafe->keyS[i], userServer);
 		keyMovement(userServerUnsafe->keyS[i], userServer);
 	}
+
+	SDL_UnlockMutex(mutex);
 }
 
 //ServerStop clears server module
 void ServerStop(void){
+	if (SDL_LockMutex(mutex) != 0){
+		puts("ServerStop: mutex lock");
+		exit(1);
+	}
+
+	networkServerStop();
+	SDL_RemoveTimer(tickId);
+
 	//exit
 	free(worldServer->exit);
 	
@@ -172,6 +239,7 @@ void ServerStop(void){
 		characterItemPrev = characterItemCurrent;
 		characterItemCurrent = characterItemCurrent->next;
 
+		//free(characterItemPrev->character.name); //it a pointer to userServer
 		free(characterItemPrev);
 	}
 
@@ -195,88 +263,68 @@ void ServerStop(void){
 		userServerItemPrev = userServerItemCurrent;
 		userServerItemCurrent = userServerItemCurrent->next;
 
+		free(userServerItemPrev->userServer.auth);
+		free(userServerItemPrev->userServer.name);
+		free(userServerItemPrev->userServer.keyS); //NULL
 		free(userServerItemPrev);
 	}
+
+	SDL_DestroyMutex(mutex);
 }
 
 //ServerConnect register new connection user, returns it with auth
 void ServerConnect(UserServer* userServerUnsafe){
 	//[R]check if game is running
 
-	//copy
-	UserServerItem* userServerItem = (UserServerItem*) malloc(sizeof(UserServerItem));
+	if (SDL_LockMutex(mutex) != 0){
+		SDL_Log("ServerConnect: mutex lock: %s", SDL_GetError());
+		exit(1);
+	}
+
+	//userServer insert
+	UserServerItem* userServerItem = userServerItemSInsert(&userServerItemS, &(UserServer){
+		.auth = NULL,
+		.character = NULL,
+		.keyS = NULL,
+		.keySLength = 0,
+		.name = (char*) malloc((15 + 1) * sizeof(char)),
+	});
 	UserServer* userServer = &(userServerItem->userServer);
-	userServer->auth = (char*) malloc((26 + 1) * sizeof(char)); //30years to crack
-	userServer->character = NULL;
-	userServer->keyS = NULL;
-	userServer->keySLength = 0;
-	userServer->name = (char*) malloc((15 + 1) * sizeof(char));
 	strncpy(userServer->name, userServerUnsafe->name, 15);
 	userServer->name[16] = '\0';
 
 	//id generate
 	while (true){
-		for(int i=0; i<26; i++){
-			userServer->auth[i] = rand() % ('Z' - 'A' + 1) + 'A';
-		}
-		userServer->auth[26] = '\0';
+		char* auth = ServerAuthCreate();
 		
-		UserServerItem* userServerItemCurrent = userServerItemS;
-		while(userServerItemCurrent != NULL){
-			if(strcmp(userServerItemCurrent->userServer.auth, userServer->auth) == 0){
-				break;
-			}		
-			userServerItemCurrent = userServerItemCurrent->next;
-		}
-
-		if(userServerItemCurrent == NULL){
+		//id exists
+		if(ServerAuthCheck(auth) == NULL){
+			userServer->auth = auth;
 			break;
 		}
+
+		free(auth);
 	}
-
-	//insert
-	if(userServerItemS == NULL){
-		userServerItem->next = NULL;
-		userServerItem->prev = NULL;
-
-		userServerItemS = userServerItem;
-	} else {
-		userServerItem->next = userServerItemS;
-		userServerItem->prev = NULL;
-		
-		userServerItemS->prev = userServerItem;
-		
-		userServerItemS = userServerItem;
-	}
-
-	//character creater
-	Character character;
-	character.bomb = 1;
-	character.position = (Position) {1 * squaresize, 1 * squaresize};
-	character.type = CharacterTypeUser;
-	character.velocity = velocity;
-	character.name = userServer->name;
 
 	//character insert
-	CharacterItem* characterItem = (CharacterItem*) malloc(sizeof(CharacterItem));
-	characterItem->character = character;
-	if(worldServer->characterItemS == NULL){
-		characterItem->next = NULL;
-		characterItem->prev = NULL;
-
-		worldServer->characterItemS = characterItem;
-	}else{
-		characterItem->next = worldServer->characterItemS;
-		characterItem->prev = NULL;
-
-		worldServer->characterItemS->prev = characterItem;
-
-		worldServer->characterItemS = characterItem;
-	}
+	CharacterItem* characterItem = characterItemSInsert(&(worldServer->characterItemS), &(Character){
+		.bomb = 1,
+		.name = userServer->name,
+		.position = (Position) {1 * squaresize, 1 * squaresize},
+		.type = CharacterTypeUser,
+		.velocity = velocity,
+	});
 
 	//character associate
 	userServer->character = &(characterItem->character);
 
 	//reply
+	free(userServerUnsafe->auth); //in best case it's free(NULL)
+	userServerUnsafe->auth = (char*) malloc((26 + 1) * sizeof(char));
 	strcpy(userServerUnsafe->auth, userServer->auth);
+
+	if(SDL_UnlockMutex(mutex) < 0){
+		SDL_Log("ServerConnect: mutex unlock: %s", SDL_GetError());
+		exit(1);
+	}
 }
