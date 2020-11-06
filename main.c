15b479fb7cb247f6c10cda9102313303da4ff242
asztrikx <asztrik35@gmail.com>
2020-11-06 19:32:27 +0100

    added start arguments

--- main.c
+++ main.c
@@ -5,27 +5,54 @@
 #include "server.h"
 #include "time.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
 	srand(time(0)); //[R] better seed
 
-	SDLInit();
+	//local
+	if(argc < 2){
+		//client
+		SDLInit();
 
-	ServerStart();
-	ClientStart();
-	ClientConnect();
+		ServerStart();
+		ClientStart();
+		ClientConnect();
 
-	//key press
-	SDL_Event sdl_event;
-	while (SDL_WaitEvent(&sdl_event) && sdl_event.type != SDL_QUIT) {
-		if(sdl_event.type == SDL_KEYDOWN || sdl_event.type == SDL_KEYUP){
-			ClientEventKey(sdl_event);
+		//key press
+		SDL_Event sdl_event;
+		while (SDL_WaitEvent(&sdl_event) && sdl_event.type != SDL_QUIT) {
+			if(sdl_event.type == SDL_KEYDOWN || sdl_event.type == SDL_KEYUP){
+				ClientEventKey(sdl_event);
+			}
 		}
+		//[R] close error not responding
+
+		ServerStop();
+		ClientStop();
+		
+		//TTF_Quit();
+		SDL_Quit();
+	} else if(strcmp(argv[1], "server") == 0){
+		ServerStart();
+
+		while(getchar() != EOF){
+			puts("Send EOF to stop server");
+		}
+
+		ServerStop();
+	} else if(strcmp(argv[1], "client") == 0){
+		ClientStart();
+		ClientConnect();
+
+		//key press
+		SDL_Event sdl_event;
+		while (SDL_WaitEvent(&sdl_event) && sdl_event.type != SDL_QUIT) {
+			if(sdl_event.type == SDL_KEYDOWN || sdl_event.type == SDL_KEYUP){
+				ClientEventKey(sdl_event);
+			}
+		}
+
+		ClientStop();
 	}
 
-	ServerStop();
-	ClientStop();
-	
-	//TTF_Quit();
-	SDL_Quit();
 	return 0;
 }
