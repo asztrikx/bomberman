040395921715a3175739bdac5bc46dcc88446db8
asztrikx <asztrik35@gmail.com>
2020-11-11 14:30:13 +0100

    added auto extending build

---
+++ .vscode/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd /mnt/hdd/Programming/C/temp/nhf/
+location=$(pwd)'/'
+regex='s:./:'$location':'
+
+gcc \
+-g \
+-Wall \
+-pedantic \
+-lm \
+-Werror \
+`sdl2-config --cflags --libs` \
+\
+-lSDL2_gfx \
+-lSDL2_ttf \
+-lSDL2_image \
+-lSDL2_mixer \
+\
+main.c \
+$(find . -name '*.c' | sed 's:./main.c::' | sed $regex | tr '\n' ' ') \
+\
+-o \
+main \
