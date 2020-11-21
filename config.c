bb2986a5ce0db823913cc42cb231b3ff7a647fc5
asztrikx <asztrik35@gmail.com>
2020-11-21 17:51:51 +0100

    modified config values

--- config.c
+++ config.c
@@ -4,13 +4,13 @@
 
 int squaresize = 50;
 int velocity = 6;
-int velocityEnemy = 3;
+int velocityEnemy = 2;
 int windowHeight = 480;
 int windowWidth = 640;
-int worldHeight = 17;
-int worldWidth = 57;
-double boxRatio = 0.4;
-double enemyRatio = 0.05;
-double enemyKeyChangePossibility = 0.02;
+int worldHeight = 13;
+int worldWidth = 21;
+double boxRatio = 0.25;
+double enemyRatio = 0.03;
+double enemyKeyChangePossibility = 0.015;
 const unsigned int tickRate = 1000u/58u;
 const long long tickSecond = 1000u/tickRate; //tick count in one second
